7e0b0e355b3360bce60c22e6e894726e6ad66e84
Wang Huizhe <huizhewang@outlook.com>
2025-07-24 18:20:42 +0800

    fix(matrix,SinkMX): properly handle channel A and C racing in SinkMX
    Originally, when A is Matrix Put message but C is also valid, the
    Matrix Put will flow into MainPipe through channel A, which is not
    allowed by Coupled L2. We should stall Matrix Put in channel A
    until channel C is free.
    
    The ready logic also takes corresponding changes.

--- src/main/scala/coupledL2/SinkMX.scala
+++ src/main/scala/coupledL2/SinkMX.scala
@@ -37,6 +37,11 @@ class SinkMX(implicit p: Parameters) extends L2Module {
   val a = io.a.bits
   val c = io.c.bits
 
+  // Determine out_a.valid:
+  // 1. if a is not a matrix put, flow io.a to out_a
+  // 2. if a is a matrix put, and in_c is not valid, flow io.a to out_c, handled above
+  // 3. if a is a matrix put, and in_c is valid, stall a
+
   // Handle MatrixGet
   when(isMatrixGet(a) && io.a.valid) {
     out_a.bits.opcode := io.a.bits.opcode//Get Or AcquireBlock
@@ -45,6 +50,10 @@ class SinkMX(implicit p: Parameters) extends L2Module {
 
   // Handle MatrixPut
   when(isMatrixPut(a) && io.a.valid) {
+    // By anycase, CoupledL2 cannot accept Put from A channel.
+    // Wait for C channel be spare.
+    out_a.valid := false.B
+
     when(!io.c.valid) {
       // If c is not valid, convert the request to ReleaseData
       out_c.bits.opcode := ReleaseData
@@ -56,7 +65,6 @@ class SinkMX(implicit p: Parameters) extends L2Module {
       out_c.bits.corrupt := a.corrupt
     //   out_c.bits.user(VaddrKey) := a.address
       out_c.valid := true.B
-      out_a.valid := false.B
     //   io.a.ready := false.B // Hold the A channel until the C channel is processed
     }
   }
@@ -68,5 +76,6 @@ class SinkMX(implicit p: Parameters) extends L2Module {
   // Handle ready signals
   // io.a.ready := out_a.ready
   io.c.ready := out_c.ready
-  io.a.ready := out_a.ready && out_c.ready
+  // Bypass channel A matrix put to C, but stalled for original C channel requests.
+  io.a.ready := Mux(isMatrixPut(a), out_c.ready && !io.c.valid, out_a.ready)
 }
